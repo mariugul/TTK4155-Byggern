d5d909a81398e254f1a3ca65f7876636ed3f300d
mariugul <32439744+mariugul@users.noreply.github.com>
2019-10-01 14:53:13 +0200

    Updated gal.vhd
    Suspicious that the "oled_dc <=" in the process of the gal.vhd was wrong. Updated it.

--- Node 1 - Breadboard/gal_decoder.vhd
+++ Node 1 - Breadboard/gal_decoder.vhd
@@ -13,14 +13,14 @@ entity address_decoder is
         ram_cs : Out std_logic;
         adc_cs : Out std_logic;
         oled_cs : Out std_logic;
-        oled_dc : Out std_logic -- Not necessary…
+        oled_dc : Out std_logic 
     );
     
     attribute LOC : string;
     attribute LOC of ram_cs     : signal is "P19";
     attribute LOC of adc_cs     : signal is "P18";
     attribute LOC of oled_cs    : signal is "P17";
-    attribute LOC of oled_dc    : signal is "P16"; -- Not necessary…
+    attribute LOC of oled_dc    : signal is "P16"; 
     attribute LOC of a11        : signal is "P1";
     attribute LOC of a10        : signal is "P2";
     attribute LOC of a9         : signal is "P3";
@@ -28,13 +28,13 @@ entity address_decoder is
 end address_decoder;
 
 architecture behave of address_decoder is begin
-    -- implement the functionality here
+    -- functionality here
     process(a11, a10, a9, a8)
     begin
         -- Active low
         oled_cs <= NOT (NOT a11 AND NOT a10);
         adc_cs  <= NOT (NOT a11 AND a10);
         ram_cs  <= NOT (a11);
-        oled_dc <= a9;
+        oled_dc <= NOT a11 AND NOT a10 AND a9;  --oled_dc <= a9; (this was the old code)
     end process;
 end behave;
