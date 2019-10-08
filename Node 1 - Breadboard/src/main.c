312061071974129c18ee72684b6285ea6e2f1b7a
Daniel Rahme <daniel.rahme.dr@gmail.com>
2019-10-08 14:03:18 +0200

    Compiling menu oled

--- Node 1 - Breadboard/src/main.c
+++ Node 1 - Breadboard/src/main.c
@@ -3,19 +3,18 @@
  *
  * Created: 10.09.2019 21:04:55
  * Author : Marius C. K. Gulbrandsen
+ *          Daniel Rahme
  */
 
 #include <util/delay.h>
 #include <avr/io.h>                     // AVR library for IO
 #include <stdio.h>                      // Standard library
 #include <stdlib.h>
-#include <stdbool.h>
 #include "uart.h"
 #include "adc.h"
-#include "oled.h"
-#include "menu.h"
 #include "xmem.h"
 #include "gpio.h"
+#include "menu.h"
 
 //unsigned char A [8] = {0b01111100,0b01111110,0b00010011,0b00010011,0b01111110,0b01111100,0b00000000,0b00000000};    // A
 
@@ -28,11 +27,10 @@ int main(void)
 	xmem_init();
 	uart_init();
 	oled_init();
-	menu_init();
-	//menu_print();
-    
-	
-	/*
+	//oled_menu_init();
+    DDRB  &= ~(1 << PB3);              // Port B3 is an input
+    int read_pin = PORTB & (1 << 3);              // Port B0 is HIGH: Turn on LED
+		
 	menu_print();
     menu_highlight(PLAY);
 
@@ -51,34 +49,18 @@ int main(void)
         const menu_state current_state = menu_highlight_handler(NOTHING);
         menu_selection(current_state);
     }
-	*/
 
     // Main program loop
     while (1) 
-	{	
-		 
-		 printf("B1: %d,   B2: %d   B3: %d\n", gpio_read_button(1), gpio_read_button(2), gpio_read_button(3));
-		
-        
-        /*
-		int but1, but2;
-		but1 = PIND & ~0b11110111;
-		but2 = PIND & ~0b11111011;
-		pos_t pos = pos_read();
-		
-		enum direction x = j_pos(pos.joy_x, pos.joy_y);
-		char dir;
-		switch (x)
-		{
-			case 1: dir = 'L'; break;
-			case 2: dir = 'R'; break;
-			case 3: dir = 'U'; break;
-			case 4: dir = 'D'; break;
-			default: dir = 'N'; break;
-		}
-		
-		printf("JX: %d%%,    JY: %d%%,    SL: %d%%,    SR: %d%%    ,Dir: %d    but1: %d,    but2: %d\n", pos.joy_x, pos.joy_y, pos.slider_l, pos.slider_r, x, but1, but2);
-		*/
+	{
+        // TODO: Read joystick pos
+
+        // Button press not working
+        volatile const int button_press = PORTB & (1 << 3); // Port B0 is HIGH: Turn on LED
+        if (button_press) {
+            const menu_state current_state = menu_highlight_handler(NOTHING);
+            menu_selection(current_state);
+        }
     }
 }
 
