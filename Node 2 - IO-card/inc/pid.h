6b0c738ea290c035ffb0d3b5a39617c17e8b085b
mariugul <32439744+mariugul@users.noreply.github.com>
2019-11-19 11:43:02 +0100

    Update

--- Node 2 - IO-card/inc/pid.h
+++ Node 2 - IO-card/inc/pid.h
@@ -22,15 +22,10 @@
 // Function Prototypes
 //---------------------------------------------------
 void PID_Init();
-void PID_Get_Motor_Pos(); // Gets the amount of motor rotations
-void PID_Error_Calc();
-void PID_Update_Last_Error();
-void PID_Proportional_Calc();
-void PID_Integral_Calc();
-void PID_Derivative_Calc();
+void PID_Update_Current_Pos(); // Gets the amount of motor rotations
+void PID_Update_Target_Pos(uint8_t joystick_pos);
 void PID_Calc(); // Calculates the speed
-void PID_Update_Pos(uint8_t joystick_pos);
-direction_t PID_Get_Direction(); // Returns the direction that was calculated
 uint8_t PID_Get_Speed();         // Returns the speed that was calculated
+direction_t PID_Get_Direction(); // Returns the direction that was calculated
 
 #endif
